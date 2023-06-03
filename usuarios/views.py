26a3bb966f6e07a52e3b385a320dc97f18aa2cc0
Damián Carnevali <damiancarnevali@hotmail.com>
2023-06-03 13:46:54 -0300

    vista usuario abierta

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,10 +2,12 @@ from rest_framework import viewsets, permissions
 from .serializers import UsuarioSerializer
 from .models import Usuario
 
+
 class UsuariosViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -13,5 +15,3 @@ class UsuariosViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-
