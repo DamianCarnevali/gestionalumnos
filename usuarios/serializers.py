7a65ac0b9740e68a3dd305deb554c0ce69c8a143
Damián Carnevali <damiancarnevali@hotmail.com>
2023-06-03 14:54:48 -0300

    model usuario

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -4,7 +4,7 @@ from .models import Usuario
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = ['email', 'nombre', 'apellido', 'password']
+        fields = '__all__'
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
